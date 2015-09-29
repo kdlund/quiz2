a1caf4cab6170946f4a44a7c45358a96db3b4ea9
kdlund <kristendlund@gmail.com>
2015-09-28 22:31:22 -0600

    Quiz 2 Complete

--- problems/array-sum.js
+++ problems/array-sum.js
@@ -1,7 +1,15 @@
 'use strict';
 
 function arraySum(arr) {
+  var totalSum = 0;
+  for (var i = 0; i < arr.length; i++) {
+    if (typeof arr[i]=="object") {
+      arr[i] = arraySum(arr[i]);
 
+    }
+    totalSum  = totalSum + arr[i];
+  }
+  return totalSum;
 }
 
 module.exports = arraySum;
--- problems/longest-string.js
+++ problems/longest-string.js
@@ -1,6 +1,15 @@
 'use strict';
 
 function longestString(arr) {
+  var longest = "";
+  for (var i = 0; i <= arr.length; i++) {
+    if (typeof arr[i]=="string") {
+      if (arr[i].length > longest.length) {
+        longest = arr[i];
+      }
+    }
+  }
+    return longest;
 
 }
 
